a1c771e2a0d25040fb330b16c434198cfaf47e6c
Aditya Verma <aaadarths2357@gmail.com>
2020-11-20 18:21:11 +0530

    Added: script to implement remove action to delete a contact from the address book list and the local storage on clicking the delete action icon

--- js/home_script.js
+++ js/home_script.js
@@ -30,11 +30,21 @@ const createInnerHtml = () => {
             <td>${contactData._zip}</td>
             <td>${contactData._phoneNumber}</td>
             <td>
-                <img name="${contactData._id}" onclick="remove(this)" alt="delete" src="..//assets/icons/delete-black-18dp.svg">
-                <img name="${contactData._id}" onclick="update(this)" alt="edit" src="..//assets/icons/create-black-18dp.svg">
+                <img id="${contactData._id}" onclick="remove(this)" alt="delete" src="..//assets/icons/delete-black-18dp.svg">
+                <img id="${contactData._id}" onclick="update(this)" alt="edit" src="..//assets/icons/create-black-18dp.svg">
             </td>
         </tr>
         `;
     }
     document.querySelector("#display").innerHTML = innerHtml;
+};
+
+const remove = (node) => {
+    let contactData = addressBookContactList.find(contact => contact._id == node.id);
+    if (!contactData) return;
+    const index = addressBookContactList.map(contact => contact._id).indexOf(contactData._id);
+    addressBookContactList.splice(index, 1);
+    localStorage.setItem("AddressBookList", JSON.stringify(addressBookContactList));
+    document.querySelector(".person-count").textContent = addressBookContactList.length;
+    createInnerHtml();
 };
